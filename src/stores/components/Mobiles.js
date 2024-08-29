da62489f270e35a84598fb7a20d6cdccc9da38e8
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-08-30 00:17:25 +0530

    first commit

---
+++ src/stores/components/Mobiles.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mobileData } from '../data/mobiles';
+import { Link } from 'react-router-dom';
+
+const Mobiles = () => {
+  const firstFiveImages = mobileData.slice(0, 5);
+
+  return (
+    <>
+      <div className="propage">
+        <div className="proTitle">
+          <h2>Mobiles</h2>
+        </div>
+        <div className="proSection">
+  {firstFiveImages.map((item, index) => (
+    <div className="imgBox" key={index}>
+      <Link to={`/mobiles/${item.id}`} className="noDecoration">
+        <img className="proImage" src={item.image} alt={`${item.company} ${item.model}`} />
+        <h2 className="company model">{item.company} {item.model}</h2>
+        <h2 className="price">₹{item.price}</h2>
+           </Link>
+    </div>
+  ))}
+</div>
+
+      </div>
+    </>
+  );
+};
+
+export default Mobiles;
+
+
