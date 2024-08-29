7d6bdc4fb0b5f4190ac8f42f8f4a81d57009ec21
E Sai harshith <142712608+Saiharshith17@users.noreply.github.com>
2024-08-30 00:23:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.js'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './stores/context/CartContext.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+import './index.css';
+import { HashRouter } from 'react-router-dom';  // Change to HashRouter
+import { CartProvider } from './stores/context/CartContext.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-  <React.StrictMode>
-    <CartProvider>
-    <App />
-    </CartProvider>
-  </React.StrictMode>,
-</BrowserRouter>
-)
+  <HashRouter>
+    <React.StrictMode>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </React.StrictMode>
+  </HashRouter>
+);
